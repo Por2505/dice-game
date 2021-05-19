e19bd8be8baf173eb168b67462e1a105e2a581b6
Por2505 <s6035512061@phuket.psu.ac.th>
2021-05-19 10:24:28 +0700

    dom

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.foor(Math.random()*6)+1;
+
+var randomImage = "dice" + randomNumber1 +".png";
+
+var source = "images/" + randonImage;
+
+var img1 = Document.querySelectorAll("img")[0];
+
+img1.setAttribute("src",source);
+
+
+
+var randomNumber1 = Math.foor(Math.random()*6)+1;
+
+var img2 = Document.querySelectorAll("img")[1];
+
+img2.setAttribute("src",source);
+
+
+
