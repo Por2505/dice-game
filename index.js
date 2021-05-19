d47ed64e8519d84653270acf512934d017c15006
Por2505 <s6035512061@phuket.psu.ac.th>
2021-05-19 10:34:00 +0700

    complete

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ var randomImage = "dice" + randomNumber1 +".png";
 
 var source = "images/" + randonImage;
 
-var img1 = Document.querySelectorAll("img")[0];
+var img1 = document.querySelectorAll("img")[0];
 
 img1.setAttribute("src",source);
 
 
 
-var randomNumber1 = Math.foor(Math.random()*6)+1;
+var randomNumber2 = Math.foor(Math.random()*6)+1;
 
-var img2 = Document.querySelectorAll("img")[1];
+var img2 = document.querySelectorAll("img")[1];
 
 img2.setAttribute("src",source);
 
 
 
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+}
+else if (randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML ="🚩 Play 2 Wins!";
+}
+else{
+    document.querySelector("h1").innerHTML =" Draw!";
+}
